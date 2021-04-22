1ed2f15ced0f521395ded3eee3149cb8ccd394fb
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2021-04-22 21:22:22 +0900

    Added input parsing, increased reset time to 3600s

--- README.md
+++ README.md
@@ -3,11 +3,15 @@ A Discord Bot for the Cambridge Sixth Term Examination Paper (STEP)
 
 Created with Python using PostgreSQL (psycopg2)
 
-Emojis will become dysfunctional 5 minutes after the message is sent. This is necessary to make sure the bot is not overwhelmed with data
+Emojis will become dysfunctional 60 minutes after the message is sent. This is necessary to make sure the bot is not overwhelmed with data
 
 ## Available Commands
 * `!s` to retrieve a question e.g. `!s 99 2 3` to get 1999 STEP 2 Q3. You can then mark the question as complete/incomplete by reacting appropriately.
 * `!c` to mark a question as complete eg `!c 99 2 3`
 * `!uc` to mark a question as incomplete eg `!uc 99 2 3`
 
-**Do not put dashes, letters (e.g. `S2`) or anything else.**
+The bot will accept the following input:
+* `1999 S2 Q4`
+* `99 2 4`, i.e. 1999 S2 Q4
+* `3/3/4`, i.e. 2003 S3 Q4
+* `09-S2-Q4`, i.e. 2009 S2 Q4
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import aiohttp
 from io import BytesIO
 import psycopg2
 import time, asyncio
+import re
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -51,15 +52,50 @@ class Listener:
     def __repr__(self):
         return "{}\n{}\n{}\n{}\n{}\n{}".format(self.k, self.author, self.edit_message, self.message, self.question, self.time)
 
+def process_input(question): #Union[str, Tuple[str]] -> Union[List[str], False]
+    max_year = 18 #latest year on STEP database is 2018 as of 04/22/2021
+
+    if len(question) == 1:
+        question = question[0]
+    if isinstance(question, str):
+        question = question.replace("-", " ")
+        question = question.replace("/", " ")
+        question = question.split(" ")
+    try:
+        year, paper, q = question
+    except ValueError: #incorrect number of args
+        return False
+
+    #alternatively, could use Python 3.9's str.removeprefix() method to remove S/s and Q/q specifically
+    # e.g. paper = paper.removeprefix("S")
+    paper = re.sub("[^0-9]", "", paper) #substitute all non-numerals with empty string
+    q = re.sub("[^0-9]", "", q)
+
+    try:
+        _year = int(year)
+        if 0 <= _year <= max_year: #years 2000-max_year
+            _year += 2000
+        elif 87 <= _year <= 99: #years 1987 - 1999
+            _year += 1900
+
+        #STEP 1 has been discontinued from 2019
+        if not (1987 <= _year <= 2018 and int(paper) in [1, 2, 3]) or (2019 <= _year <= max_year and int(paper) in [2, 3]):
+            print("Not valid paper")
+            return False
+    except ValueError:
+        return False
+
+    return list(map(str, [_year, paper, q]))
+
 class Step(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.current = []
 
-    async def check_listeners(self):
+    async def check_listeners(self): #clear reactions after 3600 seconds
         i = 0
         for start_time in [listener.time for listener in self.current]:
-            if time.time() - start_time > 300:
+            if time.time() - start_time > 3600:
                 i += 1
 
         for listener in self.current[:i]:
@@ -76,10 +112,13 @@ class Step(commands.Cog):
 
     @commands.command()
     async def s(self, ctx, *args):
-        year = args[0]
-        paper = args[1]
-        question = args[2]
-        url = f"https://stepdatabase.maths.org/database/db/{year}/{year}-S{paper}-Q{question}.png"
+        # year, paper, question = process_input(args)
+        if k := process_input(args):
+            year, paper, question = k
+        else:
+            return await ctx.channel.send("Invalid arguments")
+        _year = year[-2:] #last two digits of the year e.g. 01 from 2001
+        url = f"https://stepdatabase.maths.org/database/db/{_year}/{_year}-S{paper}-Q{question}.png"
 
         embed=discord.Embed(title="", description=f"Checking for {ctx.author}", color=0x00484F)
         embed.add_field(name="Year", value=year, inline=True)
@@ -121,55 +160,40 @@ class Step(commands.Cog):
 
     @commands.command(aliases=["c"])
     async def complete(self, ctx, *args):
-        try:
-            year = args[0]
-            paper = args[1]
-            question = args[2]
-        except IndexError:
-            return await ctx.send("Insufficient number of arguments")
-        except TypeError:
-            return await ctx.send("Need arguments")
+        # year, paper, question = process_input(args)
+        if k := process_input(args):
+            year, paper, question = k
+        else:
+            return await ctx.channel.send("Invalid arguments")
 
         with conn:
             with conn.cursor() as cur:
                 cur.execute("SELECT completed FROM members WHERE id = %s", (ctx.author.id, ))
                 a = cur.fetchone()
 
-                if 87 <= int(year) <= 99:
-                    _year = int(year) + 1900
-                elif 0 <= int(year) <= 17:
-                    _year = int(year) + 2000
-
                 if f"{year} {paper} {question}" not in a[0]: #if user has not already completed the paper
-                    if (1987 <= _year <= 2018 and int(paper) in [1, 2, 3]) or (_year >= 2019 and int(paper) in [2, 3]): #STEP 1 has been discontinued from 2019
-                        cur.execute("UPDATE members SET completed = %s WHERE id = %s", (a[0]+f"{year} {paper} {question}", ctx.author.id))
-                        await ctx.send(f"{_year} S{paper} Q{question} has been marked as complete for {ctx.author.name}.")
+                    cur.execute("UPDATE members SET completed = %s WHERE id = %s", (a[0]+f"{year} {paper} {question}", ctx.author.id))
+                    await ctx.send(f"{year} S{paper} Q{question} has been marked as complete for {ctx.author.name}.")
+                else:
+                    await ctx.send(f"{year} S{paper} Q{question} is already complete for {ctx.author.name}.")
 
     @commands.command(aliases=["uc"])
     async def uncomplete(self, ctx, *args):
-        try:
-            year = args[0]
-            paper = args[1]
-            question = args[2]
-        except IndexError:
-            return await ctx.send("Insufficient number of arguments")
-        except TypeError:
-            return await ctx.send("Need arguments")
+        if k := process_input(args):
+            year, paper, question = k
+        else:
+            return await ctx.channel.send("Invalid arguments")
 
         with conn:
             with conn.cursor() as cur:
                 cur.execute("SELECT completed FROM members WHERE id = %s", (ctx.author.id, ))
                 a = cur.fetchone()
 
-                if 87 <= int(year) <= 99:
-                    _year = int(year) + 1900
-                elif 0 <= int(year) <= 17:
-                    _year = int(year) + 2000
-
                 if f"{year} {paper} {question}" in a[0]: #if user has already completed the paper
-                    if (1987 <= _year <= 2018 and int(paper) in [1, 2, 3]) or (_year >= 2019 and int(paper) in [2, 3]): #STEP 1 has been discontinued from 2019
-                        cur.execute("UPDATE members SET completed = %s WHERE id = %s", (a[0].replace(f"{year} {paper} {question}",""), ctx.author.id))
-                        await ctx.send(f"{_year} S{paper} Q{question} has been marked as incomplete for {ctx.author.name}.")        
+                    cur.execute("UPDATE members SET completed = %s WHERE id = %s", (a[0].replace(f"{year} {paper} {question}",""), ctx.author.id))
+                    await ctx.send(f"{year} S{paper} Q{question} has been marked as incomplete for {ctx.author.name}.")
+                else:
+                    await ctx.send(f"{year} S{paper} Q{question} is already incomplete for {ctx.author.name}.")
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -192,7 +216,6 @@ class Step(commands.Cog):
                     if user.id == listener.author.id:
                         new_content = listener.edit_message.content.replace("❌", "✅")
                         await listener.edit_message.edit(content=new_content)
-
 
 bot.add_cog(Step(bot))
 bot.run(TOKEN)
